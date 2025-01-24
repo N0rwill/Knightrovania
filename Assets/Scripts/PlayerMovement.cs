b7ea089c3ac0b857c78e7906498a5f5752a7f44a
Norwill <willeverett206@gmail.com>
2025-01-24 17:40:39 -0500

    Added animation to movement
    Added animation when player runs and jumps.

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -6,9 +6,12 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     private float horizontal;
-    private float speed = 4f;
-    private float jumpingPower = 12f;
+    private float speed;
+    private float jumpingPower;
     private bool isFacingRight = true;
+    public bool isMoving;
+    private bool isGrounded;
+    private Animator anim;
 
     [SerializeField] private Rigidbody2D rb;
     [SerializeField] private Vector2 boxSize;
@@ -16,23 +19,30 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField] private Transform groundCheck;
     [SerializeField] private LayerMask groundLayer;
 
+    private void Start()
+    {
+        speed = 4f;
+        jumpingPower = 14f;
+        anim = GetComponent<Animator>();
+    }
+
     private void Update()
     {
         //Get player movement
         horizontal = Input.GetAxis("Horizontal");
 
         //jump if grounded
-        if(Input.GetButtonDown("Jump") && IsGrounded())
+        if(Input.GetButtonDown("Jump") && isGrounded == true)
         {
             rb.velocity = new Vector2(rb.velocity.x, jumpingPower);
-        }
-        //stop moving up when let go of jump
-        if(Input.GetButtonUp("Jump") && rb.velocity.y > 0f)
-        {
-            rb.velocity = new Vector2(rb.velocity.x, rb.velocity.y * 0.5f);
+            anim.Play("Jump");
         }
 
+        anim.SetFloat("AirSpeedY", rb.velocity.y);
+
         Flip();
+        AnimationController();
+        GroundChecker();
     }
 
     private void FixedUpdate()
@@ -41,18 +51,24 @@ public class PlayerMovement : MonoBehaviour
         rb.velocity = new Vector2(horizontal * speed, rb.velocity.y);
     }
 
-    private bool IsGrounded()
+    private void GroundChecker()
     {
         //ground check
-        // return Physics2D.OverlapCircle(groundCheck.position, 0.2f, groundLayer);
         if (Physics2D.BoxCast(transform.position, boxSize, 0, -transform.up, castDistance, groundLayer))
         {
-            return true;
+            isGrounded = true;
+            anim.SetBool("isGrounded", isGrounded);
         }
         else
         {
-            return false;
+            isGrounded = false;
+            anim.SetBool("isGrounded", !isGrounded);
         }
+    }
+
+     private void OnDrawGizmos()
+    {
+        Gizmos.DrawWireCube(transform.position-transform.up * castDistance, boxSize);
     }
 
     private void Flip()
@@ -67,8 +83,10 @@ public class PlayerMovement : MonoBehaviour
         }
     }
 
-     private void OnDrawGizmos()
+    private void AnimationController()
     {
-        Gizmos.DrawWireCube(transform.position-transform.up * castDistance, boxSize);
+        isMoving = rb.velocity.x != 0;
+        anim.SetBool("isMoving", isMoving);
     }
+
 }
