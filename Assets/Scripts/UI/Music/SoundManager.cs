6fa3200f0a95ebb7aebc3245df719f9d18e7b70f
tkpleo <tkristofferpurdy@gmail.com>
2025-02-06 13:34:13 -0500

    Room setup done

--- Assets/Scripts/UI/Music/SoundManager.cs
+++ Assets/Scripts/UI/Music/SoundManager.cs
@@ -13,7 +13,7 @@ public class SoundManager : MonoBehaviour
 
         if (!PlayerPrefs.HasKey("musicVolume"))
         {
-            PlayerPrefs.SetFloat("musicVolume", 0.3f);
+            PlayerPrefs.SetFloat("musicVolume", 1f);
         }
         else
         {
