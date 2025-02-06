f4009e409629ce2c4e872390c7a20d0144decae3
tkpleo <tkristofferpurdy@gmail.com>
2025-02-06 14:50:40 -0500

    AHHHHHHHHHHHHHHHHHHHHHHHH

--- Assets/Scripts/UI/StartMenu.cs
+++ Assets/Scripts/UI/StartMenu.cs
@@ -2,11 +2,20 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.Windows;
 
 public class StartMenu : MonoBehaviour
 {
     public void StartGame()
     {
         SceneManager.LoadSceneAsync("Room_1");
+    }
+
+    private void Update()
+    {
+        if (UnityEngine.Input.GetKeyDown(KeyCode.Return))
+        {
+            SceneManager.LoadSceneAsync("Room_1");
+        }
     }
 }
