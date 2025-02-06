fb51a7154000b6793db6949aec1784cc2392edaa
Cel Lewis <149333423+auxpatch@users.noreply.github.com>
2025-02-06 10:34:38 -0500

    UI Works In Rooms
    UI Fixed to work in rooms, gameover works too. Changed some stuff to make game build proper

--- Assets/Scripts/GameManager_3.cs
+++ Assets/Scripts/GameManager_3.cs
@@ -1,6 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
-using UnityEditor.U2D.Animation;
+using UnityEngine.U2D.Animation;
 using UnityEngine;
 
 public class GameManager_3 : MonoBehaviour
